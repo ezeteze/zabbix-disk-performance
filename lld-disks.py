2201eaa170c89d7d0634ed26b0c2265593d1d983
Christos Sidiropoulos <csidiropoulos@upcom.eu>
2013-12-13 12:02:03 +0200

    Change to support Python 2.6.

--- lld-disks.py
+++ lld-disks.py
@@ -3,6 +3,23 @@
 import json
 import subprocess
 
+# Nasty hack for 2.6 Python.
+if "check_output" not in dir( subprocess ): # duck punch it in!
+    def f(*popenargs, **kwargs):
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed, it will be overridden.')
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise subprocess.CalledProcessError(retcode, cmd)
+        return output
+    subprocess.check_output = f
+
+
 if __name__ == '__main__':
     output = subprocess.check_output("cat /proc/diskstats | awk '{print $3}' | grep -v 'ram\|loop\|sr'", shell=True)
     data = list()
